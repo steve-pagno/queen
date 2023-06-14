b55818c3cf9e94999be2e9a581a46ce37d536e69
Steve Pagno <105939669+steve-pagno@users.noreply.github.com>
2023-06-14 17:37:17 -0300

    Add files via upload

---
+++ js/sideNav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNav").style.top = "0px";
+    document.getElementById("open").style.visibility = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.top = "-180px";
+    document.getElementById("open").style.visibility = "visible"
+}
